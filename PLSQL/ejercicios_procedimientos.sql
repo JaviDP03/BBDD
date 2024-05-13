1bc2851ff26d64984ebd294795a8de0a661261e3
JaviDP03 <javiduarte34@gmail.com>
2024-05-13 09:47:39 +0200

    Ejercicios condicionales y bucles

--- PLSQL/ejercicios_procedimientos.sql
+++ PLSQL/ejercicios_procedimientos.sql
@@ -69,26 +69,26 @@ SELECT @tipo_nota;
 -- 6. Resuelva el procedimiento diseñado en el ejercicio anterior haciendo uso de la estructura de control CASE.
 DELIMITER $$
 DROP PROCEDURE IF EXISTS tipo_nota_alumno3$$
-CREATE PROCEDURE tipo_nota_alumno3(IN nota_alumno FLOAT)
+CREATE PROCEDURE tipo_nota_alumno3(IN nota_alumno FLOAT, OUT tipo_nota VARCHAR(20))
 	BEGIN
-    SELECT DISTINCT
 		CASE
 			WHEN (nota_alumno <= 10 AND nota_alumno >= 9)
-				THEN 'Sobresaliente'
+				THEN SET tipo_nota = 'Sobresaliente';
 			WHEN (nota_alumno < 9 AND nota_alumno >= 7)
-				THEN 'Notable'
+				THEN SET tipo_nota = 'Notable';
 			WHEN (nota_alumno < 7 AND nota_alumno >= 6)
-				THEN 'Bien'
+				THEN SET tipo_nota = 'Bien';
 			WHEN (nota_alumno < 6 AND nota_alumno >= 5)
-				THEN 'Suficiente'
+				THEN SET tipo_nota = 'Suficiente';
 			WHEN (nota_alumno < 5 AND nota_alumno >= 0)
-				THEN 'Insuficiente'
+				THEN SET tipo_nota = 'Insuficiente';
 			ELSE
-				'Nota no válida'
-        END AS TipoNota FROM fabricante;
+				SET tipo_nota = 'Nota no válida';
+        END CASE;
 	END$$
 DELIMITER ;
-CALL tipo_nota_alumno3(5.55);
+CALL tipo_nota_alumno3(5.55, @tipo_nota);
+SELECT @tipo_nota;
 
 -- 7. Escribe un procedimiento que reciba como parámetro de entrada un valor numérico que represente un día de la semana y
 -- que devuelva una cadena de caracteres con el nombre del día de la semana correspondiente.
@@ -122,30 +122,30 @@ SELECT @nombre_dia;
 -- 8. Resuelva el procedimiento diseñado en el ejercicio anterior haciendo uso de la estructura de control CASE.
 DELIMITER $$
 DROP PROCEDURE IF EXISTS nombre_dia_semana2$$
-CREATE PROCEDURE nombre_dia_semana2(IN numero_dia FLOAT)
+CREATE PROCEDURE nombre_dia_semana2(IN numero_dia FLOAT, OUT nombre_dia VARCHAR(20))
 	BEGIN
-		SELECT DISTINCT
-        CASE
+		CASE
 			WHEN (numero_dia = 1)
-				THEN 'Lunes'
+				THEN SET nombre_dia = 'Lunes';
 			WHEN (numero_dia = 2)
-				THEN 'Martes'
+				THEN SET nombre_dia = 'Martes';
 			WHEN (numero_dia = 3)
-				THEN 'Miercoles'
+				THEN SET nombre_dia = 'Miercoles';
 			WHEN (numero_dia = 4)
-				THEN 'Jueves'
+				THEN SET nombre_dia = 'Jueves';
 			WHEN (numero_dia = 5)
-				THEN 'Viernes'
+				THEN SET nombre_dia = 'Viernes';
 			WHEN (numero_dia = 6)
-				THEN 'Sábado'
+				THEN SET nombre_dia = 'Sábado';
 			WHEN (numero_dia = 7)
-				THEN 'Domingo'
+				THEN SET nombre_dia = 'Domingo';
 			ELSE
-				'Día no válido'
-			END AS NombreDia FROM fabricante;
+				SET nombre_dia = 'Día no válido';
+			END CASE;
 	END$$
 DELIMITER ;
-CALL nombre_dia_semana2(4);
+CALL nombre_dia_semana2(4, @nombre_dia);
+SELECT @nombre_dia;
 
 -- 1.8.2
 -- 1. Escribe un procedimiento que reciba el nombre de un país como parámetro de entrada y
@@ -194,6 +194,188 @@ CREATE PROCEDURE datos_por_metodo_pago
 DELIMITER ;
 CALL datos_por_metodo_pago('PayPal', @pago_maximo, @pago_minimo, @media_pagos, @suma_pagos, @numero_pagos);
 SELECT @pago_maximo, @pago_minimo, @media_pagos, @suma_pagos, @numero_pagos;
+
+-- 4. Crea una base de datos llamada procedimientos que contenga una tabla llamada cuadrados.
+-- La tabla cuadrados debe tener dos columnas de tipo INT UNSIGNED, una columna llamada número y otra columna llamada cuadrado.
+-- Una vez creada la base de datos y la tabla deberá crear un procedimiento llamado calcular_cuadrados con las siguientes características.
+-- El procedimiento recibe un parámetro de entrada llamado tope de tipo INT UNSIGNED y calculará el valor de los cuadrados de
+-- los primeros números naturales hasta el valor introducido como parámetro. El valor del números y de sus cuadrados deberán
+-- ser almacenados en la tabla cuadrados que hemos creado previamente.
+-- Tenga en cuenta que el procedimiento deberá eliminar el contenido actual de la tabla antes de insertar los nuevos valores de los cuadrados que va a calcular.
+-- Utilice un bucle WHILE para resolver el procedimiento.
+DROP DATABASE IF EXISTS procedimientos;
+CREATE DATABASE procedimientos;
+USE procedimientos;
+CREATE TABLE cuadrados (
+	numero INT UNSIGNED PRIMARY KEY,
+    cuadrado INT UNSIGNED
+);
+DELIMITER $$
+DROP PROCEDURE IF EXISTS calcular_cuadrados$$
+CREATE PROCEDURE calcular_cuadrados(IN tope INT UNSIGNED)
+	BEGIN
+		DECLARE contador_numeros INT DEFAULT 1;
+		DELETE FROM cuadrados;
+        WHILE contador_numeros <= tope DO
+			INSERT INTO cuadrados VALUES (contador_numeros, POWER(contador_numeros, 2));
+            SET contador_numeros = contador_numeros + 1;
+		END WHILE;
+    END$$
+DELIMITER ;
+CALL calcular_cuadrados(5);
+SELECT * FROM cuadrados;
+
+-- 6. Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.
+DROP DATABASE IF EXISTS procedimientos;
+CREATE DATABASE procedimientos;
+USE procedimientos;
+CREATE TABLE cuadrados (
+	numero INT UNSIGNED PRIMARY KEY,
+    cuadrado INT UNSIGNED
+);
+DELIMITER $$
+DROP PROCEDURE IF EXISTS calcular_cuadrados2$$
+CREATE PROCEDURE calcular_cuadrados2(IN tope INT UNSIGNED)
+	BEGIN
+		DECLARE contador_numeros INT DEFAULT 1;
+		DELETE FROM cuadrados;
+        bucle_numeros: LOOP
+			INSERT INTO cuadrados VALUES (contador_numeros, POWER(contador_numeros, 2));
+            IF contador_numeros = tope THEN
+				LEAVE bucle_numeros;
+			END IF;
+            SET contador_numeros = contador_numeros + 1;
+		END LOOP bucle_numeros;
+    END$$
+DELIMITER ;
+CALL calcular_cuadrados2(10);
+SELECT * FROM cuadrados;
+
+-- 7. Crea una base de datos llamada procedimientos que contenga una tabla llamada ejercicio.
+-- La tabla debe tener una única columna llamada número y el tipo de dato de esta columna debe ser INT UNSIGNED.
+-- Una vez creada la base de datos y la tabla deberá crear un procedimiento llamado calcular_números con las siguientes características.
+-- El procedimiento recibe un parámetro de entrada llamado valor_inicial de tipo INT UNSIGNED y deberá almacenar en la tabla ejercicio
+-- toda la secuencia de números desde el valor inicial pasado como entrada hasta el 1.
+-- Tenga en cuenta que el procedimiento deberá eliminar el contenido actual de las tablas antes de insertar los nuevos valores.
+-- Utilice un bucle WHILE para resolver el procedimiento.
+DROP DATABASE IF EXISTS procedimientos;
+CREATE DATABASE procedimientos;
+USE procedimientos;
+CREATE TABLE ejercicio (
+	numero INT UNSIGNED PRIMARY KEY
+);
+DELIMITER $$
+DROP PROCEDURE IF EXISTS calcular_numeros$$
+CREATE PROCEDURE calcular_numeros(IN valor_inicial INT UNSIGNED)
+	BEGIN
+		DECLARE contador_numeros INT;
+        SET contador_numeros = valor_inicial;
+		DELETE FROM ejercicio;
+        WHILE contador_numeros >= 1 DO
+			INSERT INTO ejercicio VALUES (contador_numeros);
+            SET contador_numeros = contador_numeros - 1;
+		END WHILE;
+    END$$
+DELIMITER ;
+CALL calcular_numeros(8);
+SELECT * FROM ejercicio;
+
+-- 9. Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.
+DROP DATABASE IF EXISTS procedimientos;
+CREATE DATABASE procedimientos;
+USE procedimientos;
+CREATE TABLE ejercicio (
+	numero INT UNSIGNED PRIMARY KEY
+);
+DELIMITER $$
+DROP PROCEDURE IF EXISTS calcular_numeros2$$
+CREATE PROCEDURE calcular_numeros2(IN valor_inicial INT UNSIGNED)
+	BEGIN
+		DECLARE contador_numeros INT;
+        SET contador_numeros = valor_inicial;
+		DELETE FROM ejercicio;
+		bucle_numeros: LOOP
+			INSERT INTO ejercicio VALUES (contador_numeros);
+            IF contador_numeros = 1 THEN
+				LEAVE bucle_numeros;
+			END IF;
+            SET contador_numeros = contador_numeros - 1;
+		END LOOP bucle_numeros;
+    END$$
+DELIMITER ;
+CALL calcular_numeros2(8);
+SELECT * FROM ejercicio;
+
+-- 10. Crea una base de datos llamada procedimientos que contenga una tabla llamada pares y otra tabla llamada impares.
+-- Las dos tablas deben tener única columna llamada número y el tipo de dato de esta columna debe ser INT UNSIGNED.
+-- Una vez creada la base de datos y las tablas deberá crear un procedimiento llamado calcular_pares_impares con las siguientes características.
+-- El procedimiento recibe un parámetro de entrada llamado tope de tipo INT UNSIGNED y deberá almacenar en la tabla pares aquellos números pares
+-- que existan entre el número 1 el valor introducido como parámetro. Habrá que realizar la misma operación para almacenar los números impares en la tabla impares.
+-- Tenga en cuenta que el procedimiento deberá eliminar el contenido actual de las tablas antes de insertar los nuevos valores.
+-- Utilice un bucle WHILE para resolver el procedimiento.
+DROP DATABASE IF EXISTS procedimientos;
+CREATE DATABASE procedimientos;
+USE procedimientos;
+CREATE TABLE pares (
+	numero INT UNSIGNED PRIMARY KEY
+);
+CREATE TABLE impares (
+	numero INT UNSIGNED PRIMARY KEY
+);
+DELIMITER $$
+DROP PROCEDURE IF EXISTS calcular_pares_impares$$
+CREATE PROCEDURE calcular_pares_impares(IN tope INT UNSIGNED)
+	BEGIN
+		DECLARE contador_numeros INT DEFAULT 1;
+		DELETE FROM pares;
+        DELETE FROM impares;
+        WHILE contador_numeros <= tope DO
+			IF (contador_numeros % 2 = 0) THEN
+				INSERT INTO pares VALUES (contador_numeros);
+			ELSE
+				INSERT INTO impares VALUES (contador_numeros);
+			END IF;
+            SET contador_numeros = contador_numeros + 1;
+		END WHILE;
+    END$$
+DELIMITER ;
+CALL calcular_pares_impares(15);
+SELECT * FROM pares;
+SELECT * FROM impares;
+
+-- 12. Utilice un bucle LOOP para resolver el procedimiento del ejercicio anterior.
+DROP DATABASE IF EXISTS procedimientos;
+CREATE DATABASE procedimientos;
+USE procedimientos;
+CREATE TABLE pares (
+	numero INT UNSIGNED PRIMARY KEY
+);
+CREATE TABLE impares (
+	numero INT UNSIGNED PRIMARY KEY
+);
+DELIMITER $$
+DROP PROCEDURE IF EXISTS calcular_pares_impares2$$
+CREATE PROCEDURE calcular_pares_impares2(IN tope INT UNSIGNED)
+	BEGIN
+		DECLARE contador_numeros INT DEFAULT 1;
+		DELETE FROM pares;
+        DELETE FROM impares;
+        bucle_numeros: LOOP
+			IF (contador_numeros % 2 = 0) THEN
+				INSERT INTO pares VALUES (contador_numeros);
+			ELSE
+				INSERT INTO impares VALUES (contador_numeros);
+			END IF;
+            IF contador_numeros = tope THEN
+				LEAVE bucle_numeros;
+			END IF;
+            SET contador_numeros = contador_numeros + 1;
+		END LOOP bucle_numeros;
+    END$$
+DELIMITER ;
+CALL calcular_pares_impares2(13);
+SELECT * FROM pares;
+SELECT * FROM impares;
 
 -- 1.8.3
 -- 2. Escribe un procedimiento que devuelva el valor de la hipotenusa de un triángulo a partir de los valores de sus lados.
